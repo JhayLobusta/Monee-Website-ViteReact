87f773ef96597c1f6723ae44a2d1c14f182698c8
JhayLobusta <contact.jlobusta@gmail.com>
2023-12-01 09:32:13 +0800

    Added Tailwinfd css

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import moneeLogoBlack from './assets/monee-logo.png';
 import googleLogo from './assets/google.svg';
 import appleLogo from './assets/apple.svg';
 import mobileMockup from './assets/mockup.png';
+import { Container } from 'postcss';
 
 const App = () => {
   return (
-    <>
-      <nav>
-        <div className="logo">
-          <img src={moneeLogoBlack} alt="monee" className="img-logo" />
-          Monee
-        </div>
-        <div className="nav-items">
-          <a href="#"><i className="fa-brands fa-facebook"></i></a>
-          <a href="#"><i className="fa-brands fa-discord"></i></a>
-        </div>
-      </nav>
+    <><div className='h-screen mx-auto pt-12 max-w-screen-xl'>
+    <header>
+    <nav className="flex justify-between items-center">
+  
+  <div className="flex items-center text-left">
+    <img src={moneeLogoBlack} alt="Logo" className="h-10 w-10 mr-6"/>
+    <span className="text-2xl font-semibold text-black">Monee</span>
+  </div>
 
-      <section className="hero">
-        <div className="hero-container">
-          <div className="column-left">
-            <h1>Your Personal Financial Sidekick!</h1>
-            <p>
-              Effortlessly keep tabs on your finances, whether it's tracking your expenses and income, setting budgets, or saving for your money goals.
-            </p>
-            <div className="download-button">
-              <a href="#" className="button-container">
-                <img src={googleLogo} alt="Google Logo" />
-                <div className="overlay">Coming Soon</div>
-              </a>
-              <a href="" className="button-container">
-                <img src={appleLogo} alt="Apple Logo" />
-                <div className="overlay">Coming Soon</div>
-              </a>
-            </div>
-          </div>
-          <div className="column-right">
+  
+  <div class="flex space-x-4">
+    <a href="#"><i className="text-black fa-brands fa-facebook"></i></a>
+    <a href="#"><i className="text-black fa-brands fa-discord"></i></a>
+  </div>
+</nav>
+</header>
+
+<section className="flex items-center gap-8 mt-24">
+  {/* Hero Left column */}
+  <div className="flex-1 space-y-12">
+    <h1 className="text-5xl font-medium mb-4 text-black">Your Personal Financial Sidekick!</h1>
+    <p className="text-md text-black">Effortlessly keep tabs on your finances, whether it's tracking your expenses and income, setting budgets, or saving for your money goals.</p>
+    <div className="flex flex-row space-x-8">
+      <a href="#" className="button-container">
+        <img src={googleLogo} alt="Google Logo" />
+        <div className="overlay">Coming Soon</div>
+      </a>
+      <a href="#" className="button-container">
+        <img src={appleLogo} alt="Apple Logo" />
+        <div className="overlay">Coming Soon</div>
+      </a>
+    </div>
+  </div>
+
+  {/* Hero Right column */}
+  <div className="flex-1 text-left flex items-center justify-center">
             <div className="circle-back"></div>
             <div className="circle-front">
               <img src={mobileMockup} alt="mockup" className="hero-image" />
             </div>
-          </div>
-        </div>
-      </section>
+  </div>
+</section>
 
-      <section className="section-2">
-        <h2>Discover the Power of Monee</h2>
-        <br />
-        <div className="section-2-container">
-          {/* ... (similar structure for each half) */}
-        </div>
-        <div className="section-2-container">
-          {/* ... (similar structure for each half) */}
-        </div>
-      </section>
 
-      <div className="section-3">
+      {/* <div className="section-3">
         <div className="section-3-left">
           <div className="circle"></div>
         </div>
@@ -68,26 +60,7 @@ const App = () => {
           <p>Send us a message for collaboration and inquiries. Watch your ideas come to life as we build something extraordinary together!</p>
           <button className="send-email"> Send Email</button>
         </div>
-      </div>
-
-      <div className="section-4">
-        <div className="section-4-left">
-          <img src={moneeLogoBlack} alt="monee" className="img-logo-footer" />
-          <div className="div-about">
-            <h3>Monee</h3>
-            {/* ... (about content) */}
-          </div>
-        </div>
-        <div className="section-4-right">
-          <div className="div-quick-links">
-            <h3>Quick Links</h3>
-            {/* ... (quick links content) */}
-          </div>
-          <div className="div-support">
-            <h3>Support</h3>
-            {/* ... (support content) */}
-          </div>
-        </div>
+      </div> */}
       </div>
     </>
   );
