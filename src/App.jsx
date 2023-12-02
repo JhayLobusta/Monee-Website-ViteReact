ab020c8011a199a60c9fcb68b1885b7c8da14c2a
JhayLobusta <contact.jlobusta@gmail.com>
2023-12-02 09:00:46 +0800

    Update Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { Container } from 'postcss';
 const App = () => {
   return (
     <>
-    <div className=' mx-auto pt-12 max-w-screen-xl'>
+    <div className=' mx-6 pt-12 max-w-screen-xl sm:mx-10 md:mx-10 lg:mx-16 xl:24 2xl:mx-auto'>
     <div className='h-screen'>
     <header>
     <nav className="flex justify-between items-center">
@@ -29,17 +29,17 @@ const App = () => {
 </nav>
 </header>
 
-<section className="flex items-center gap-8 mt-24">
+<section className="flex sm:flex-col md:flex-col lg:flex-row xl:flex-row 2xl:flex-row items-center gap-8 sm:gap-0 md:gap-0 mt-24">
   {/* Hero Left column */}
   <div className="flex-1 space-y-12">
     <h1 className="text-5xl font-medium mb-4 text-black">Your Personal Financial Sidekick!</h1>
     <p className="text-md text-gray-600">Effortlessly keep tabs on your finances, whether it's tracking your expenses and income, setting budgets, or saving for your money goals, especially handy when abroad or working internationally!</p>
-    <div className="flex flex-row space-x-8">
+    <div className="flex sm:flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row space-x-0 sm:space-x-0 md:space-x-8 lg:space-x-8 xl:space-x-8 2xl:space-x-8 space-y-0 sm:space-y-8 md:space-y-0 lg:space-y-0 xl:space-y-0 2xl:space-y-0">
       <a href="#" className="button-container">
-        <div className="overlay bg-gradient-to-r transition-all duration-400 ease-in-out from-slate-950 to-slate-950 hover:from-slate-950 hover:to-lime-700"><svg className='w-6 h-6 mr-2' xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#ffffff" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/></svg>Coming Soon</div>
+        <div className="overlay bg-gradient-to-r w-auto h-16 sm:w-auto md:w-56 lg:w-56 transition-all ease-in-out from-slate-950 to-slate-950 hover:from-slate-950 hover:to-lime-700"><svg className='w-6 h-6 mr-2' xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path fill="#ffffff" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/></svg>Coming Soon</div>
       </a>
       <a href="#" className="button-container">
-        <div className="overlay bg-gradient-to-r transition-all duration-400 ease-in-out from-slate-950 to-slate-950 hover:from-slate-950 hover:to-blue-700"><svg className='w-8 h-8 mr-2 mb-1' xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path fill="#ffffff" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/></svg>Coming Soon</div>
+        <div className="overlay bg-gradient-to-r w-auto h-16 sm-w-auto md:w-56 lg:w-56 transition-all ease-in-out from-slate-950 to-slate-950 hover:from-slate-950 hover:to-blue-700"><svg className='w-8 h-8 mr-2 mb-1' xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path fill="#ffffff" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"/></svg>Coming Soon</div>
       </a>
     </div>
   </div>
@@ -60,7 +60,7 @@ const App = () => {
     <div class="flex justify-start">
       <h2 class="text-2xl font-semibold tracking-tight text-gray-900 sm:text-2xl">Discover the Power of Monee</h2>
     </div>
-    <div class="mx-auto grid max-w-3xl grid-cols-2 gap-x-2 gap-y-16 sm:mt-14 sm:pt-14 lg:mx-0 lg:max-w-none lg:grid-cols-2 ">
+    <div class="mx-auto grid max-w-3xl grid-cols-1 gap-x-2 gap-y-16 sm:mt-14 sm:pt-14 sm:grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2 ">
     <article class="flex max-w-xl flex-row items-start justify-between gap-x-8 ">
 
     <div class="group relative flex-col">
@@ -72,7 +72,7 @@ const App = () => {
               Track Finances with Ease
             </a>
           </h3>
-          <a class="relative z-10 rounded-full px-3 py-1.5 text-slate-50 bg-red-400">Usage</a>
+          <a class="relative rounded-full px-3 py-1.5 text-slate-50 bg-red-400">Usage</a>
           
         </div>
           
@@ -92,7 +92,7 @@ const App = () => {
               Global Currency Converter
             </a>
           </h3>
-          <a class="relative z-10 rounded-full px-3 py-1.5 text-slate-50 bg-cyan-500">Feature</a>
+          <a class="relative rounded-full px-3 py-1.5 text-slate-50 bg-cyan-500">Feature</a>
         </div>
           
           <p class="mt-5 ml-10 line-clamp-3 text-md leading-6 text-gray-600">Going abroad or working with international clients? We've got you covered! What's even more incredible is that Monee's currency converter works seamlessly offline.</p>
@@ -110,7 +110,7 @@ const App = () => {
               Fully Customizable
             </a>
           </h3>
-          <a class="relative z-10 rounded-full px-3 py-1.5 text-slate-50 bg-violet-400">Design</a>
+          <a class="relative rounded-full px-3 py-1.5 text-slate-50 bg-violet-400">Design</a>
         </div>
           
           <p class="mt-5 ml-10 line-clamp-3 text-md leading-6 text-gray-600">Make your money management uniquely yours with Monee! Choose from lots of colors and icons to match your style and personalize your financial experience.</p>
@@ -128,10 +128,10 @@ const App = () => {
               Affordable and Ad-Free
             </a>
           </h3>
-          <a class="relative z-10 rounded-full px-3 py-1.5 text-slate-50 bg-amber-400">Price</a>
+          <a class="relative rounded-full px-3 py-1.5 text-slate-50 bg-amber-400">Price</a>
         </div>
           
-          <p class="mt-5 ml-10 line-clamp-3 text-md leading-6 text-gray-600">Monee is not only affordable, but it's also ad-free. No interruptions, just a seamless and focused experience as you take control of your finances.</p>
+          <p class="mt-5 ml-10 line-clamp-3 text-md leading-6 text-gray-600">Monee offers a free version and not only affordable, but it's also ad-free. No interruptions, just a seamless and focused experience as you take control of your finances.</p>
         </div>
       </article>
     </div>
