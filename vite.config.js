0cebd910444e0b7517ba1fe61a9f2b76c1340bae
JhayLobusta <contact.jlobusta@gmail.com>
2023-12-26 19:40:24 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Monee-Website-ViteReact/",
+export default defineConfig({ 
   plugins: [react()],
+  base: "/Monee-Website-ViteReact/",
 })
